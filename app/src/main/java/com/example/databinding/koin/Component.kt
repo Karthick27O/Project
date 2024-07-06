891c09e03953601b281fcb3854707567039ac6e1
Karthick27O <148423263+Karthick27O@users.noreply.github.com>
2024-07-06 13:06:53 +0530

    cleanUp

--- app/src/main/java/com/example/databinding/koin/Component.kt
+++ app/src/main/java/com/example/databinding/koin/Component.kt
@@ -1,11 +1,9 @@
 package com.example.databinding.koin
 
 import com.example.databinding.PrefHelper
-import org.koin.core.component.KoinApiExtension
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
 
-@OptIn(KoinApiExtension::class)
 class Component : KoinComponent
 {
      val prefHelper: PrefHelper by inject()
